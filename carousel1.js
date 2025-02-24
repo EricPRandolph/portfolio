a69565b96ed328ed457a668fabd4970ef60367f8
Zoe Rizzo <rizzo.zoej@gmail.com>
2025-02-23 19:08:04 -0500

    projects placeholders

--- carousel1.js
+++ carousel1.js
@@ -1,79 +1,38 @@
-let slideIntervalOne;
-let slideIntervalTwo;
-
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector('.prev1').addEventListener('click', () => {
-    resetIntervalOne();
-    plusDivsOne(-1);
-  });
-  document.querySelector('.next1').addEventListener('click', () => {
-    resetIntervalOne();
-    plusDivsOne(1);
-  });
+  document.querySelectorAll(".carousel").forEach((carousel, index) => {
+    let slideIndex = 1;
+    let slideInterval;
 
-  document.querySelector('.prev2').addEventListener('click', () => {
-    resetIntervalTwo();
-    plusDivsTwo(-1);
-  });
-  document.querySelector('.next2').addEventListener('click', () => {
-    resetIntervalTwo();
-    plusDivsTwo(1);
-  });
+    const slides = carousel.querySelectorAll(".mySlides");
+    const prevButton = carousel.querySelector(".prev");
+    const nextButton = carousel.querySelector(".next");
 
-  startIntervalOne();
-  startIntervalTwo();
+    function showSlides(n) {
+      if (n > slides.length) slideIndex = 1;
+      if (n < 1) slideIndex = slides.length;
+
+      slides.forEach((slide) => (slide.style.display = "none"));
+      slides[slideIndex - 1].style.display = "block";
+    }
+
+    function plusSlides(n) {
+      resetInterval();
+      showSlides((slideIndex += n));
+    }
+
+    function startInterval() {
+      slideInterval = setInterval(() => plusSlides(1), 3000);
+    }
+
+    function resetInterval() {
+      clearInterval(slideInterval);
+      startInterval();
+    }
+
+    prevButton.addEventListener("click", () => plusSlides(-1));
+    nextButton.addEventListener("click", () => plusSlides(1));
+
+    showSlides(slideIndex);
+    startInterval();
+  });
 });
-
-var slideIndexOne = 1;
-var slideIndexTwo = 1;
-showDivsOne(slideIndexOne);
-showDivsTwo(slideIndexTwo);
-
-function plusDivsOne(n) {
-  showDivsOne(slideIndexOne += n);
-}
-
-function showDivsOne(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides1");
-  if (n > x.length) {slideIndexOne = 1}
-  if (n < 1) {slideIndexOne = x.length};
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndexOne-1].style.display = "block";
-}
-
-function startIntervalOne() {
-  slideIntervalOne = setInterval(() => plusDivsOne(1), 3000);
-}
-
-function resetIntervalOne() {
-  clearInterval(slideIntervalOne);
-  startIntervalOne();
-}
-
-
-function plusDivsTwo(n) {
-  showDivsTwo(slideIndexTwo += n);
-}
-
-function showDivsTwo(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides2");
-  if (n > x.length) {slideIndexTwo = 1}
-  if (n < 1) {slideIndexTwo = x.length};
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndexTwo-1].style.display = "block";
-}
-
-function startIntervalTwo() {
-  slideIntervalTwo = setInterval(() => plusDivsTwo(1), 3000);
-}
-
-function resetIntervalTwo() {
-  clearInterval(slideIntervalTwo);
-  startIntervalTwo();
-}
